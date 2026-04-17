3e8caeb49dccd537d7c9b910e2e387cc6e76c874
skochar1 <sk1@wellesley.edu>
2026-04-17 13:09:57 -0700

    removed smth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -300,11 +300,11 @@ export default function App() {
             <div className="hidden h-px flex-1 bg-gray-200 ml-6 sm:block" />
           </div>
           <ul className="list-disc list-inside space-y-3 text-gray-700">
-            <li>Awarded the Jacobs School of Engineering Fellowship for first-year PhD funding.</li>
+            <li>Awarded the Jacobs School of Engineering Fellowship for first-year Ph.D. funding.</li>
             <li>Received a $1000 grant to attend USENIX NSDI ‘26.</li>
             <li>Appointed by Professor Norman Sadeh to join the Smart City Privacy Technologies project (funded by NSF).</li>
             <li>"Beyond Creepiness: Predictive Privacy" accepted to Privacy Law Scholars Conference (PLSC) 2025.</li>
-            <li>Accepted to attend The Cornell, Maryland, Max Planck Pre-doctoral Research School in Computer Science (CMMRS) 2025.</li>
+            <li>Accepted to the Cornell, Maryland, Max Planck Pre-doctoral Research School in Computer Science (CMMRS) 2025.</li>
             <li>Received a $750 NSF travel grant to attend ACM Symposium (CS&amp;Law '25).</li>
             <li>Selected as a Data Science Institute Scholar at Columbia. Received a $3000 research stipend.</li>
             <li>Received a $1000 NSF travel grant to attend ACM Symposium (CS&amp;Law '24).</li>
