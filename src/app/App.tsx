47906dbe98072d8ba7e9be7edfe9f183a27f69a9
skochar1 <sk1@wellesley.edu>
2026-06-10 16:58:49 -0400

    test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -58,7 +58,7 @@ export default function App() {
           <div className="flex flex-col md:flex-row gap-8 items-start">
             <div className="flex-shrink-0">
               <ImageWithFallback
-                src="IMG_4889.jpg"
+                src="IMG_8374.HEIC"
                 alt="Profile"
                 className="w-64 h-64 object-cover"
               />
